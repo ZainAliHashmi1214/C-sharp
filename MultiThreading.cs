dc7ee2c157312b841c0fc7df35c8d2274009520f
ZainAliHashmi1214 <zain921472@gmail.com>
2024-07-26 13:24:19 -0700

    Add files via upload

---
+++ MultiThreading.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Thread thread1 = new Thread(PrintNumbers);
+        thread1.Name = "Thread 1";
+
+        
+        Thread thread2 = new Thread(PrintNumbers);
+        thread2.Name = "Thread 2";
+
+        thread1.Start();
+        thread2.Start();
+
+        
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("All threads have completed their work.");
+    }
+
+
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} prints {i}");
+            Thread.Sleep(100); 
+        }
+    }
+}
